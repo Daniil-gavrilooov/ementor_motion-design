5f2e96764af045f3fcfb55c47f8d9a09403bfd94
Daniil-gavrilooov <85822019+Daniil-gavrilooov@users.noreply.github.com>
2022-08-20 19:40:47 +0300

    fics burger__menu

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,13 @@ let burgerMenu = document.querySelector('.header__burger-menu')
 let burgerBtn = document.querySelector('.header__burger-btn')
 let burgerMenuItem = document.querySelectorAll('.header__menu-item')
 let body = document.querySelector('body')
+let header = document.querySelector('.header')
 
 burgerBtn.onclick = function() {
     this.classList.toggle('header__burger-btn--active')
     burgerMenu.classList.toggle('header__burger-menu--active')
     body.classList.toggle('lock')
+    header.classList.toggle('header--active')
 }
 
 burgerMenuItem.forEach(item => item.onclick = function() {
