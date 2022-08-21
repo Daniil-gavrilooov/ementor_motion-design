8832627a47642692593c98bd6f738350ba3ec2e1
Daniil-gavrilooov <85822019+Daniil-gavrilooov@users.noreply.github.com>
2022-08-21 13:10:27 +0300

    add css  section (questions)

--- app/js/main.js
+++ app/js/main.js
@@ -15,3 +15,17 @@ burgerMenuItem.forEach(item => item.onclick = function() {
     burgerBtn.classList.remove('header__burger-btn--active')
     burgerMenu.classList.remove('header__burger-menu--active')
 })
+
+let  questionsBtn = document.querySelectorAll('.questions__item-btn')
+
+questionsBtn.forEach(btn => btn.onclick = function () {
+    let questionsButton = this.querySelector('.questions__item-button')
+    let mainItem = this.parentNode
+    let questionsText = mainItem.querySelector('.questions__item-text')
+    let questionsTitle = mainItem.querySelector('.questions__item-title')
+
+    questionsButton.classList.toggle('questions__item-button--active')
+    questionsText.classList.toggle('questions__item-text--active')
+    questionsTitle.classList.toggle('questions__item-title--active')
+
+})
